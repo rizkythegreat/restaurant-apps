9f3d46de395deec5a0cfcf70aa0e4e9d277b3868
Rizky Rahman Salam <clashikik@gmail.com>
2024-08-25 10:41:37 +0700

    feat: change server to run serve

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,19 +4,21 @@ import '../styles/responsive.css'
 import App from './views/app'
 import swRegister from './utils/sw-register'
 
-const app = new App({
-  button: document.querySelector('#hamburgerButton'),
-  drawer: document.querySelector('#navigationDrawer'),
-  content: document.querySelector('#mainContent'),
-  hero: document.querySelector('.hero')
-})
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new App({
+    button: document.querySelector('#hamburgerButton'),
+    drawer: document.querySelector('#navigationDrawer'),
+    content: document.querySelector('#mainContent'),
+    hero: document.querySelector('.hero')
+  })
 
-window.addEventListener('hashchange', () => {
-  app.renderPage()
-  swRegister()
-  window.scrollTo(0, 0)
-})
+  window.addEventListener('hashchange', () => {
+    app.renderPage()
+    swRegister()
+    window.scrollTo(0, 0)
+  })
 
-window.addEventListener('load', () => {
-  app.renderPage()
+  window.addEventListener('load', () => {
+    app.renderPage()
+  })
 })
--- tests/sampleTest.test.js
+++ tests/sampleTest.test.js
@@ -1,7 +1,33 @@
-const sum = (a, b) => a + b
+import { describe, it, expect } from '@jest/globals'
 
-describe('A Sample Test for Sum', () => {
-  it('should return a + b value', () => {
-    expect(sum(2, 3)).toEqual(5)
+const detectTriangle = (sideA, sideB, sideC) => {
+  [sideA, sideB, sideC].forEach((side) => {
+    if (!Number.isInteger(side)) {
+      throw new Error('Sides have to be Integer')
+    }
+    if (side < 1) {
+      throw new Error('Strange Triangle')
+    }
+    if (sideA === sideB && sideA === sideC) {
+      return 'Equilateral Triangle'
+    }
+  })
+}
+
+describe('Detect the triangle', () => {
+  it('Should fail if the sides are less than 1', () => {
+    expect(() => detectTriangle(-1, 2, 2)).toThrowError('Strange Triangle')
+    expect(() => detectTriangle(1, -2, 2)).toThrowError('Strange Triangle')
+    expect(() => detectTriangle(1, 2, -2)).toThrowError('Strange Triangle')
+  })
+  it('Should fail if the sides are not integer', () => {
+    expect(() => detectTriangle('a', 2, 2)).toThrowError('Sides have to be Integer')
+    expect(() => detectTriangle(1, 'a', 2)).toThrowError('Sides have to be Integer')
+    expect(() => detectTriangle(1, 2, 'a')).toThrowError('Sides have to be Integer')
+
+    expect(() => detectTriangle(' ', 2, 2)).toThrowError('Sides have to be Integer')
+  })
+  it('Should detect Equilateral Triangle if all sides are equal', () => {
+    expect(detectTriangle(1, 1, 1)).toEqual('Equilateral Triangle')
   })
 })
