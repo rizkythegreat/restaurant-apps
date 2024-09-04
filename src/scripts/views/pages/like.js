e7615f2dae07624370da20873e747b53d275b381
Rizky Rahman Salam <clashikik@gmail.com>
2024-09-04 21:59:54 +0700

    Update like.js

--- src/scripts/views/pages/like.js
+++ src/scripts/views/pages/like.js
@@ -2,7 +2,7 @@ import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb'
 import { createRestaurantTemplate } from '../templates/template-creator'
 
 const Like = {
-  async render () {
+  async render() {
     return `
          <section id="mainContent" class="restaurant-list">
             <h2>Favorite Lists</h2>
@@ -11,12 +11,17 @@ const Like = {
         </section>
         `
   },
-  async afterRender () {
+  async afterRender() {
     const restaurant = await FavoriteRestaurantIdb.getAllRestaurants()
     const restaurantContainer = document.querySelector('#restaurants')
-    restaurant.forEach((restaurant) => {
-      restaurantContainer.innerHTML += createRestaurantTemplate(restaurant)
-    })
+    
+    if (restaurant.length === 0) {
+      restaurantContainer.innerHTML = '<p>Tidak ada data</p>'
+    } else {
+      restaurant.forEach((restaurant) => {
+        restaurantContainer.innerHTML += createRestaurantTemplate(restaurant)
+      })
+    }
   }
 }
 
